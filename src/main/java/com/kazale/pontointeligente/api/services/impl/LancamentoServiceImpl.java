884dfd547896fb74d451ae405adeedc62eff7de1
Artur Costa <arturcostta@gmail.com>
2018-05-28 20:28:53 -0300

    adicionando ehcache

--- src/main/java/com/kazale/pontointeligente/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/kazale/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,7 @@ public class LancamentoServiceImpl implements LancamentoService {
 	}
 
 	@Override
+	@Cacheable("lancamentoPorId")
 	public Optional<Lancamento> buscarPorId(Long id) {
 		log.info("Buscando lançamentos por id {}",id);
 		return Optional.ofNullable( this.lancRepository.findOne(id) );
@@ -35,6 +38,7 @@ public class LancamentoServiceImpl implements LancamentoService {
 	
 
 	@Override
+	@CachePut("lancamentoPorId")
 	public Lancamento persistir(Lancamento lancamento) {
 		log.info("Persistindo um lançamento {}",lancamento);
 		return this.lancRepository.save(lancamento);
